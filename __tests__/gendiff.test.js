5b84dadcd1ad8b5fabd48ad3f539559f33ad906b
Sofya <sofya-good@mail.ru>
2020-09-22 00:18:29 +0200

    add display json format test, add getFixturePath function

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,35 @@
+import path from 'path';
+import fs from 'fs';
 import gendiff from '../src/index.js';
-import expectedString from '../__fixtures__/expectedString.js';
-import expectedPlainOutputString from '../__fixtures__/expectedPlainOutputString.js';
 
-test('comparing json', () => {
-  expect(gendiff('file1.json', 'file2.json')).toEqual(expectedString);
+const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
+const readFile = (pathToFile) => fs.readFileSync(pathToFile, 'utf-8');
+
+const expectedString = readFile(getFixturePath('expectedString.txt'));
+const expectedPlainOutputString = readFile(getFixturePath('expectedPlainOutputString.txt'));
+const expectedStringifyJsonOutput = readFile(getFixturePath('expectedStringifyJsonOutput.json'));
+
+test('comparing json files', () => {
+  const calculatedDiff = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  expect(calculatedDiff).toEqual(expectedString);
 });
 
-test('comparing yaml', () => {
-  expect(gendiff('file1.yml', 'file2.yml')).toEqual(expectedString);
+test('comparing yaml files', () => {
+  const calculatedDiff = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+  expect(calculatedDiff).toEqual(expectedString);
 });
 
-test('comparing ini', () => {
-  expect(gendiff('file1.ini', 'file2.ini')).toEqual(expectedString);
+test('comparing ini files', () => {
+  const calculatedDiff = gendiff(getFixturePath('file1.ini'), getFixturePath('file2.ini'));
+  expect(calculatedDiff).toEqual(expectedString);
 });
 
-test('plain format', () => {
-  expect(gendiff('file1.json', 'file2.json', 'plain')).toEqual(expectedPlainOutputString);
+test('display plain format', () => {
+  const calculatedDiff = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
+  expect(calculatedDiff).toEqual(expectedPlainOutputString);
+});
+
+test('display json format', () => {
+  const calculatedDiff = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
+  expect(calculatedDiff).toEqual(expectedStringifyJsonOutput);
 });
